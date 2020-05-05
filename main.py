d22b40b657acc85221936b967497eadfe225b610
Ritul Patel <Ritul.Patel@vonage.com>
2020-05-05 16:33:29 -0400

    Removed colored logs

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import os
 import sys
 from logging import Formatter
 
-import coloredlogs
-
 
 class LogFormatter(Formatter):
     def format(self, record):
@@ -22,12 +20,10 @@ logger = logging.getLogger("PyCICD")
 logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
 logger.addHandler(handler)
 
-coloredlogs.install()
-
 
 if __name__ == "__main__":
     logger.debug("Python main handler got invoked!")
-    logger.info(f"Starting ${os.environ.get('GITHUB_REPOSITORY')}-${os.environ.get('GITHUB_WORKFLOW')}:${os.environ.get('GITHUB_ACTION')}")
+    logger.info(f"Starting {os.environ.get('GITHUB_REPOSITORY')}-{os.environ.get('GITHUB_WORKFLOW')}:{os.environ.get('GITHUB_ACTION')}")
 
     logger.info(os.environ)
     logger.warning(sys.argv)
